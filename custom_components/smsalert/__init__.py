15497edcf28355c8ca0ce031cfd09d96e9d9c8d1
Alex Dinu <a.dinu@resume-library.com>
2026-02-19 11:14:33 +0200

    feat: init use constants

--- custom_components/smsalert/__init__.py
+++ custom_components/smsalert/__init__.py
@@ -2,20 +2,16 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 
-from .const import DOMAIN
-
-PLATFORMS: list[str] = ["notify"]
-
+PLATFORMS: list[Platform] = [Platform.NOTIFY]
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
